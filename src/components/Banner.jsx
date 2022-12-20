853495c237027ebb794b67e078045f1cf6d11f82
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2022-12-20 19:54:33 -0300

    primer poyecto react

---
+++ src/components/Banner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export  function Banner() {
+  return (
+    <section className="banner">
+        <h1 className="titulo">Tenemos lo mejor para ti...</h1>
+    </section>
+  )
+}
